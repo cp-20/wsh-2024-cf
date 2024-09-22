4a1708c559458fbb239dec7406c3a62a872c22b0
cp-20 <n.naoki.1587@gmail.com>
2024-09-22 10:03:04 +0900

    いらないもの消した

--- workspaces/client/src/index.tsx
+++ workspaces/client/src/index.tsx
@@ -1,6 +1,3 @@
-import './side-effects';
-
-import $ from 'jquery';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { SWRConfig } from 'swr';
@@ -8,14 +5,11 @@ import { SWRConfig } from 'swr';
 import { AdminApp } from '@wsh-2024/admin/src/index';
 import { ClientApp } from '@wsh-2024/app/src/index';
 
-import { preloadImages } from './utils/preloadImages';
 import { registerServiceWorker } from './utils/registerServiceWorker';
 
 const main = async () => {
-  await registerServiceWorker();
-  await preloadImages();
-
-  $(document).ready(() => {
+  registerServiceWorker();
+  document.addEventListener('DOMContentLoaded', () => {
     if (window.location.pathname.startsWith('/admin')) {
       ReactDOM.createRoot($('#root').get(0)!).render(<AdminApp />);
     } else {
